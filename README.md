0a49b7ea6bc79c676b3ce78330ca28ab996ed831
jessy <as1103314@gmail.com>
2023-04-05 15:29:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_works_school_second_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_works_school_second_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_works_school_second_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
